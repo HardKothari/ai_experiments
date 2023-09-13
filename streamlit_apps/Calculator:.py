2f3b9fb59021977461dd07be5a95beaa46613891
Hard Kothari <57263430+HardKothari@users.noreply.github.com>
2023-09-12 20:59:24 -0400

    requirements.txt

---
+++ streamlit_apps/Calculator:.py
@@ -0,0 +1,15 @@
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            print("Warning: Divisor cannot be zero.")
+            return None
+        return a / b
